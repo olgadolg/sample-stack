02d7b32548c1552aa6f49f0a9a3bbfd76323db70
Daniel Jansson <daniel@vinnovera.se>
2016-07-17 13:43:42 +0200

    Remove unused dependency

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,23 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux';
-import styles from '../../main.css';
 
 export class App extends Component {
-  constructor (props) {
-    super(props)
-  }
 
-  render () {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    )
-  }
+	constructor (props) {
+		super(props)
+	}
+
+	render () {
+		return (
+			<div>
+				{this.props.children}
+			</div>
+		)
+	}
 }
 
 App.propTypes = {
-  children: PropTypes.object
+	children: PropTypes.object
 }
 
 export default connect()(App)
