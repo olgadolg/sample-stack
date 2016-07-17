459b1823c57281c2e4d025172cabcc8d3aa3fafe
Daniel Jansson <daniel@vinnovera.se>
2016-07-18 00:17:48 +0200

    Added update coords action

--- src/actions/clickarea.js
+++ src/actions/clickarea.js
@@ -1,13 +1,24 @@
 import { addClickarea } from './addclickarea'
 
 export const createClickarea = (name) => (dispatch) => {
-	return dispatch(addClickarea({name}));
+	dispatch(addClickarea({name}));
 };
 
 export const updateFill = (bool) => (dispatch) => {
 	dispatch({
 		type: 'UPDATE_FILL',
 		data: bool
+	});
+
+}
+
+export const updateCoords = (coords, index) => (dispatch) => {
+
+	console.log(coords, index)
+	
+	dispatch({
+		type: 'UPDATE_COORDS',
+		data: {coords, index}
 	});
 
 }
