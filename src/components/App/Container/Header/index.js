014fe2c5f4694a975aba276dcf43c5d3ff01c891
Daniel Jansson <daniel@vinnovera.se>
2016-08-01 20:58:32 +0200

    color update

--- src/components/App/Container/Header/index.js
+++ src/components/App/Container/Header/index.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import Toolbox from '../Toolbox';
+import { SliderPicker } from 'react-color';
 import styles from './styles/styles.css';
 
 export default class Header extends Component {
 
 	render () {
 		const logo = require('../../../../images/logo.png');
+		const sliderStyle = {
+			width: '200px',
+			height: 'auto',
+			position: 'absolute',
+			right: '250px',
+			top: '7px'
+		};
 
 		return (
 			<header draggable className={styles.header}>
+				<div style={sliderStyle}>
+					<SliderPicker />
+				</div>
 				<Toolbox />
 				<img src={logo} alt="logo" />
 			</header>
